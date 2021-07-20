f3f7c822adc3e62a9f7d98298ea0b66250972468
OrenVildermanPepperi <oren.v@pepperi.com>
2021-07-20 23:10:10 +0300

    added name to variable name in the function

--- item_box_test.py
+++ item_box_test.py
@@ -3,7 +3,7 @@ import sys
 from item_box import ItemBox
 
 def test_create_item_box():
-    item_1 = ItemBox("Sauce Labs Bolt T-Shirt");
+    item_1 = ItemBox(name="Sauce Labs Bolt T-Shirt");
 
     print(item_1.name)
     assert(item_1.name == "Sauce Labs Bolt T-Shirt")
@@ -13,7 +13,7 @@ def test_create_item_box():
     assert(item_1.description == "Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.")
 
 def test_create_item_box_negative():
-    item_2 = ItemBox("Sauce Labs Bolt T-Shirt");
+    item_2 = ItemBox(name="Sauce Labs Bolt T-Shirt");
 
     print(item_2.name)
     assert(item_2.name == "Sauce Labs Bolt T-Shirt")
