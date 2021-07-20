1f1cc0866546ec18f84c96a6c75121dbf8df26e8
OrenVildermanPepperi <oren.v@pepperi.com>
2021-07-20 22:43:04 +0300

    Created Item Box class and used this class in a test with pytest

---
+++ item_box_test.py
@@ -0,0 +1,23 @@
+import pytest
+import sys
+from item_box import ItemBox
+
+def test_create_item_box():
+    item_1 = ItemBox("Sauce Labs Bolt T-Shirt");
+
+    print(item_1.name)
+    assert(item_1.name == "Sauce Labs Bolt T-Shirt")
+    print(item_1.price)
+    assert(item_1.price == 15.99)
+    print(item_1.description)
+    assert(item_1.description == "Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.")
+
+def test_create_item_box_negative():
+    item_2 = ItemBox("Sauce Labs Bolt T-Shirt");
+
+    print(item_2.name)
+    assert(item_2.name == "Sauce Labs Bolt T-Shirt")
+    print(item_2.price)
+    assert(item_2.price == 15)
+    print(item_2.description)
+    assert(item_2.description == "Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.")
